bf6c235984eaab6c1f7a478a8f9b24ee91faf9c9
Julian Pinzon <julian.pinzon@alertandote.com>
2023-08-15 20:26:29 -0500

    Se avanzó en la funcion de traducir

---
+++ js/translate.js
@@ -0,0 +1,160 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const translateButton = document.getElementById("translateButton");
+  const inicio = document.getElementById("inicio-en");
+  const contactanos = document.getElementById("contactanos-en");
+  const inicia_sesion = document.getElementById("inicia-sesion-en");
+  const adquiere_tu_fraccion = document.getElementById(
+    "adquiere-tu-fraccion-en"
+  );
+  const datos = document.getElementById("datos-en");
+  const nombre = document.getElementById("nombre-en");
+  const correo_electronico = document.getElementById("correo-electronico-en");
+  const mensaje = document.getElementById("mensaje-en");
+  const mas_informacion = document.getElementById("mas-informacion-en");
+  const visita_faq = document.getElementById("visita-faq-en");
+  const encuentra_preguntas = document.getElementById("encuentra-preguntas-en");
+  const llamanos = document.getElementById("llamanos-en");
+  const lunes_a_viernes = document.getElementById("lunes-a-viernes-en");
+  const departamentos_de_lujo = document.getElementById(
+    "departamentos_de_lujo-en"
+  );
+  const preventa = document.getElementById("preventa-en");
+  const pie_de_playa = document.getElementById("pie-de-playa-en");
+  const exclusivas_visitas_al_mar = document.getElementById(
+    "exclusivas-visitas-al-mar-en"
+  );
+  const concierge = document.getElementById("concierge-en");
+  const brochure = document.getElementById("brochure-en");
+  const descubre_precio = document.getElementById("descubre-precio-en");
+  const lujo_exclusivo = document.getElementById("lujo-exclusivo-en");
+  const lujo = document.getElementById("lujo-en");
+  const fraccion_exclusiva = document.getElementById("fraccion-exclusiva-en");
+  const confort = document.getElementById("confort-en");
+  const experiementa_el_confort = document.getElementById(
+    "experiementa-el-confort-en"
+  );
+  const servicios = document.getElementById("servicios-en");
+  const descubre_servicios = document.getElementById("descubre-servicios-en");
+  const disfruta_de_amenidades = document.getElementById(
+    "disfruta-de-amenidades-en"
+  );
+  const desde_nuestro_jungle = document.getElementById(
+    "desde-nuestro-jungle-en"
+  );
+  const no_esperes_mas = document.getElementById("no-esperes-mas-en");
+  const adquiere_tu_accion_hoy = document.getElementById(
+    "adquiere-tu-accion-hoy-en"
+  );
+  const master_plan = document.getElementById("master-plan-en");
+  const moneda_desarrollo = document.getElementById("moneda-desarrollo-en");
+  const unidad = document.getElementById("unidad-en");
+  const precio_por_lista = document.getElementById("precio-por-lista-en");
+  const area = document.getElementById("area-en");
+  const plano_exclusivo = document.getElementById("plano-exclusivo-en");
+  const plazo_de_financiamiento = document.getElementById(
+    "plazo-de-financiamiento-en"
+  );
+  const seleccione_un_plazo = document.getElementById("seleccione-un-plazo-en");
+  const diez_enganche = document.getElementById("diez-enganche-en");
+  const nueve_contado = document.getElementById("nueve-contado-en");
+  const descuento = document.getElementById("descuento-en");
+  const enganche = document.getElementById("enganche-en");
+  const total_a_financiar = document.getElementById("total-a-financiar-en");
+  const mensualidades = document.getElementById("mensualidades-en");
+  const de = document.getElementById("de-en");
+  const descargar_cotizacion = document.getElementById(
+    "descargar-cotizacion-en"
+  );
+  const enero = document.getElementById("enero-en");
+  const febrero = document.getElementById("febrero-en");
+  const marzo = document.getElementById("marzo-en");
+  const Abril = document.getElementById("Abril-en");
+  const Mayo = document.getElementById("Mayo-en");
+  const Junio = document.getElementById("Junio-en");
+  const julio = document.getElementById("julio-en");
+  const agosto = document.getElementById("agosto-en");
+  const septiembre = document.getElementById("septiembre-en");
+  const octubre = document.getElementById("octubre-en");
+  const noviembre = document.getElementById("noviembre-en");
+  const Diciembre = document.getElementById("Diciembre-en");
+  const visita_a_laguna = document.getElementById("visita-a-laguna-en");
+  const visita_al_mar = document.getElementById("visita-al-mar-en");
+  const visita_a_la_laguna = document.getElementById("visita-a-la-laguna-en");
+  const descubre_tu_nuevo = document.getElementById("descubre-tu-nuevo-en");
+  const amenidades = document.getElementById("amenidades-en");
+  const algunas_de_nuestras = document.getElementById("algunas-de-nuestras-en");
+  const descubre_todas = document.getElementById("descubre-todas-en");
+  const piscina_roof = document.getElementById("piscina-roof-en");
+  const area_de_fogata = document.getElementById("area-de-fogata-en");
+  const area_de_camastros = document.getElementById("area-de-camastros-en");
+  const piscina_recreativa = document.getElementById("piscina-recreativa-en");
+  const palapa_con_barra = document.getElementById("palapa-con-barra-en");
+  const concierge_dias_año = document.getElementById("concierge-dias-año-en");
+  const entrega = document.getElementById("entrega-en");
+  const las_mejores_comodidades = document.getElementById(
+    "las-mejores-comodidades-en"
+  );
+  const la_residencia_se_entrega = document.getElementById(
+    "la-residencia-se-entrega-en"
+  );
+  const gastos_de_escritura = document.getElementById("gastos-de-escritura-en");
+  const amueblado = document.getElementById("amueblado-en");
+  const descubre_tu_fraccion = document.getElementById(
+    "descubre-tu-fraccion-en"
+  );
+  const planes_comerciales = document.getElementById("planes-comerciales-en");
+  const texto = document.getElementById("texto-en");
+  const m2_interiores = document.getElementById("m2-interiores-en");
+  const recamaras_flex = document.getElementById("recamaras-flex-en");
+  const dos_baños = document.getElementById("dos-baños-en");
+  const terraza = document.getElementById("terraza-en");
+  const estacionamiento = document.getElementById("estacionamiento-en");
+  const completamente_vestido = document.getElementById(
+    "completamente-vestido-en"
+  );
+  const aparta_tu_ticket = document.getElementById("aparta-tu-ticket-en");
+  const interiores = document.getElementById("interiores-en");
+  const tres_recamaras = document.getElementById("tres-recamaras-en");
+  const tres_baños = document.getElementById("tres-baños-en");
+  const interiores_exteriores = document.getElementById(
+    "interiores-exteriores-en"
+  );
+  const cuatro_baños = document.getElementById("cuatro-baños-en");
+  const terraza_de = document.getElementById("terraza-de-en");
+  const completamente_amuebaldo = document.getElementById(
+    "completamente-amuebaldo-en"
+  );
+  const pueblo_magico = document.getElementById("pueblo-magico-en");
+  const minutos = document.getElementById("minutos-en");
+  const consulado_americano = document.getElementById("consulado-americano-en");
+  const isla_cabo_norte = document.getElementById("isla-cabo-norte-en");
+  const aeropuerto = document.getElementById("aeropuerto-en");
+  const paseo_montejo = document.getElementById("paseo-montejo-en");
+  const aviso_de_privacidad = document.getElementById("aviso-de-privacidad-en");
+
+  const translations = {
+    en: {
+      inicio: "Start",
+    },
+    es: {
+      inicio: "Inicio",
+    },
+  };
+
+  let currentLanguage = "es";
+
+  translateButton.addEventListener("click", function () {
+    toggleLanguage();
+  });
+
+  function toggleLanguage() {
+    currentLanguage = currentLanguage === "en" ? "es" : "en";
+    updateTexts();
+  }
+
+  function updateTexts() {
+    inicio.textContent = translations[currentLanguage].inicio;
+  }
+
+  updateTexts();
+});
